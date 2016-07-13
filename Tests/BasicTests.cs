a75e17554665cb2ca2b917f13fb58ec0f39c1b1c
Daniel Barowy <dbarowy@cs.umass.edu>
2016-07-13 18:48:58 -0400

    Import Depends tests

--- Tests/BasicTests.cs
+++ Tests/BasicTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
 {
@@ -10,14 +8,13 @@ namespace Tests
         [TestMethod]
         public void checkForVLOOKUP()
         {
-            // (1,1)    =VLOOKUP(B1,$C$1:$D$5,2,FALSE)]}
-
+            // (1,1)    =VLOOKUP(B1,$C$1:$D$5,2,FALSE)
             var filename = @"..\..\TestData\VHLOOKUP.xlsx";
             var app = new COMWrapper.Application();
             var wb = app.OpenWorkbook(filename);
             var dag = wb.buildDependenceGraph();
 
-            var addr_a1 = AST.Address.fromA1(1, "A", wb.WorksheetName(1), wb.WorkbookName, wb.Path);
+            var addr_a1 = AST.Address.fromA1withMode(1, "A", AST.AddressMode.Absolute, AST.AddressMode.Absolute, wb.WorksheetName(1), wb.WorkbookName, wb.Path);
             var frm_a1 = dag.getFormulaAtAddress(addr_a1);
 
             Assert.AreEqual("=VLOOKUP(B1,$C$1:$D$5,2,FALSE)", frm_a1);
@@ -26,14 +23,13 @@ namespace Tests
         [TestMethod]
         public void checkForHLOOKUP()
         {
-            // (1,6)    =HLOOKUP(B1,G1:K2,2,FALSE)]}
-
+            // (1,6)    =HLOOKUP(B1,G1:K2,2,FALSE)
             var filename = @"..\..\TestData\VHLOOKUP.xlsx";
             var app = new COMWrapper.Application();
             var wb = app.OpenWorkbook(filename);
             var dag = wb.buildDependenceGraph();
 
-            var addr_a1 = AST.Address.fromA1(1, "F", wb.WorksheetName(1), wb.WorkbookName, wb.Path);
+            var addr_a1 = AST.Address.fromA1withMode(1, "F", AST.AddressMode.Absolute, AST.AddressMode.Absolute, wb.WorksheetName(1), wb.WorkbookName, wb.Path);
             var frm_a1 = dag.getFormulaAtAddress(addr_a1);
 
             Assert.AreEqual("=HLOOKUP(B1,G1:K2,2,FALSE)", frm_a1);
